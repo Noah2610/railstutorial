af3c73c1372d71b982caf55859dcca6c929757d3
Noah Rosenzweig <noah@rays-lab.com>
2017-07-26 18:27:52 +0200

    Added dynamic User show page with Gravatar

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-	root "static_pages#home"
-  get '/help'			=>	'static_pages#help'	
-  get '/about'		=>	'static_pages#about'	
-  get '/contact'	=>	'static_pages#contact'
-  get '/signup'		=>	'user#new'
+	root 'static_pages#home'
+	get  '/help',     to:  'static_pages#help'
+	get  '/about',    to:  'static_pages#about'
+	get  '/contact',  to:  'static_pages#contact'
+	get  '/signup',   to:  'users#new'
+	resources :users
+
 end
