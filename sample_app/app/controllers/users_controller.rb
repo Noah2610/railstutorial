18217214bb48f4b308d4020498878ba072f6e316
Noah Rosenzweig <noah@rays-lab.com>
2017-07-26 19:30:25 +0200

    Finished working signup page with redirects and error feedback

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if (@user.save)
-			# handle saving user to db
+			redirect_to @user
 		else
 			render "new"
 		end
