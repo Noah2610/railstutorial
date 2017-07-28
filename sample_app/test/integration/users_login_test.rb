5cb0c980b438041910d051a626610d2ab6a7d8ac
Noah Rosenzweig <noah@rays-lab.com>
2017-07-28 18:56:26 +0200

    Added logout functionality; basic login complete

--- sample_app/test/integration/users_login_test.rb
+++ sample_app/test/integration/users_login_test.rb
@@ -8,24 +8,32 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
 	test "login with invalid information" do
 		get login_path
-		assert_template "sessions/new"
+			assert_template "sessions/new"
 		post login_path, params: { session: { email: "", password: "" } }
-		assert_template "sessions/new"
-		assert_select "div.alert-danger"
+			assert_template "sessions/new"
+			assert_select "div.alert-danger"
 		get root_path
-		assert flash.empty?
+			assert flash.empty?
 	end
 
-	test "login with valid information" do
+	test "login with valid information followed by logout" do
 		post login_path, params: { session: { email: @user.email, password: "password" } }
-		assert_redirected_to @user
-		follow_redirect!
-		assert_template "users/show"
-		assert_select "div.alert-danger", count: 0
-		assert_select "div.alert-success"
-		assert_select "a[href=?]", login_path, count: 0
-		assert_select "a[href=?]", logout_path
-		assert_select "a[href=?]", user_path(@user)
+			assert is_logged_in?
+			assert_redirected_to @user
+			follow_redirect!
+			assert_template "users/show"
+			assert_select "div.alert-danger", count: 0
+			assert_select "div.alert-success"
+			assert_select "a[href=?]", login_path, count: 0
+			assert_select "a[href=?]", logout_path
+			assert_select "a[href=?]", user_path(@user)
+		delete logout_path
+			assert_not is_logged_in?
+			assert_redirected_to root_path
+			follow_redirect!
+			assert_select "a[href=?]", login_path
+			assert_select "a[href=?]", logout_path, count: 0
+			assert_select "a[href=?]", user_path(@user), count: 0
 	end
 
 end
