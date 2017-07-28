e7a18b9c417204dc6e46bcb3f87301a5c0eb4aee
Noah Rosenzweig <noah@rays-lab.com>
2017-07-28 16:37:49 +0200

    Added login feature with session storage, no visible effects yet

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if (user) && (user.authenticate(params[:session][:password]))
-			# handle user login
+			log_in user
+			redirect_to user  # rails automatically converts this to user_url(user)
 		else
 			flash.now[:danger] = "Invalid email / password combination."
 			render 'new'
