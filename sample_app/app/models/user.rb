7658c481638742fd1d7af0a057f2ddb81f7aaae8
Noah Rosenzweig <noah@rays-lab.com>
2017-07-31 14:34:58 +0200

    Added permanent stay logged in cookie, no checkbox for user yet

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 	before_save { self.email.downcase! }
 	#VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	VALID_EMAIL_REGEX = /\A[\w+\-]+(\.[\w+\-]+)*[\w+\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -18,4 +20,27 @@ class User < ApplicationRecord
 			BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)
 	end
+
+	# Returns a random token:
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	# Remember user in the database for persistent login:
+	def remember
+		self.remember_token = User.new_token
+		update_attribute :remember_digest, User.digest(remember_token)
+	end
+
+	# Returns true if the given token matches the digest:
+	def authenticated?(remember_token)
+		return false  if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	# Forgest a user, for logging out:
+	def forget
+		update_attribute :remember_digest, nil
+	end
+
 end
