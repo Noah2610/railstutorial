fb6ca2eac7057e66bf03561cfa4240ab79cb3f55
Noah Rosenzweig <noah@rays-lab.com>
2017-07-31 20:16:25 +0200

    Added will_paginate for users index page

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -6,6 +6,17 @@ class UserControllerTest < ActionDispatch::IntegrationTest
 		@other_user = users(:archer)
 	end
 
+	test "should get users index when logged in" do
+		log_in_as @user
+		get users_path
+		assert_template "users/index"
+	end
+
+	test "should redirect index when not logged in" do
+		get users_path
+		assert_redirected_to login_url
+	end
+
 	test "should get new" do
 		get signup_path
 		assert_response :success
--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+	def setup
+		@user = users(:michael)
+	end
+
 	test "layout links" do
 		get root_path
 			assert_template 'static_pages/home'
@@ -9,6 +13,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 			assert_select "a[href=?]", about_path
 			assert_select "a[href=?]", contact_path
 			assert_select "a[href=?]", signup_path
+			assert_select "a[href=?]", login_path
 		get help_path
 			assert_select "title", full_title("Help")
 		get about_path
@@ -17,6 +22,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 			assert_select "title", full_title("Contact")
 		get signup_path
 			assert_select "title", full_title("Sign up")
+
+		log_in_as @user
+		get root_path
+			assert_select "a[href=?]", users_path
+			assert_select "a[href=?]", login_path, count: 0
+			assert_select "a[href=?]", logout_path
+			assert_select "a[href=?]", user_path(@user)
+			assert_select "a[href=?]", edit_user_path(@user)
+		get edit_user_path(@user)
+			assert_select "a[href=?]", "http://gravatar.com/emails"
 
 	end
 end
