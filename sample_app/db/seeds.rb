3782d4e606a9af2704764c4eb688487cca67ce3d
Noah Rosenzweig <noah@rays-lab.com>
2017-08-02 13:59:37 +0200

    Microposts are now rendered on user profiles

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -13,7 +13,7 @@ User.create!(	name:  "Example User",
 							password_confirmation: "foobar",
 							admin: true )
 
-99.times do |n|
+98.times do |n|
 	name  = Faker::Name.name
 	email = "example-#{n+1}@railstutorial.org"
 	password = "password"
@@ -23,9 +23,20 @@ User.create!(	name:  "Example User",
 							 password_confirmation: password)
 end
 
-users = User.order(:created_at).take(6)
+User.create!(	name:  "Chuck Norris",
+							email: "chuck@norris.me",
+							password:              "foobar",
+							password_confirmation: "foobar",
+							admin: true )
+
+users = User.order(:created_at).take(5)
 50.times do
 	content = Faker::Lorem.sentence(5)
 	users.each { |user| user.microposts.create!(content: content) }
 end
 
+user = User.find_by(name: "Chuck Norris")
+60.times do
+	content = Faker::ChuckNorris.fact.truncate(140)
+	user.microposts.create!(content: content)
+end
