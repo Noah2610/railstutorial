7730b684f29a50dae343c4633c60c1766ae25f28
Noah Rosenzweig <noah@rays-lab.com>
2017-08-02 13:05:43 +0200

    Added micropost model and properly associated it with user model; wrote corresponding tests; no client interactions yet

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -74,4 +74,12 @@ class UserTest < ActiveSupport::TestCase
 		assert_not @user.authenticated?('')
 	end
 
+	test "associated microposts should be destroyed" do
+		@user.save
+		@user.microposts.create content: "test content"
+		assert_difference "Micropost.count", -1 do
+			@user.destroy
+		end
+	end
+
 end
